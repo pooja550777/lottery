91d4cd1b8de3ab82c3d43cd3f533aa69f6a3faac
POOJA <jfsjd.mum.pooja@gmail.com>
2022-11-29 20:02:20 +0530

    lottery commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const randomone = document.getElementById('one');
+const randomtwo = document.getElementById('two');
+const randomthree = document.getElementById('three');
+const btn = document.getElementById('btn');
+
+btn.addEventListener('clilck' function()
+{
+
+    Numberone();
+    Numbertwo();
+    Numberthree();
+})
+
+function Numberone(){
+    randomone.value = (math.floor (math.random() *10;))
+}
+
+function Numbertwo(){
+    randomone.value = (math.floor (math.random() *10;))
+}
+function Numbethree(){
+    randomone.value = (math.floor (math.random() *10;))
+}
